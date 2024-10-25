ba48895cd07791af77b08329c335179bd7fb943f
Hugo Solorza <solorza.hugo.6800@eam.edu.co>
2024-10-25 11:42:38 -0500

    CREACION clase figuras con dos metodos

---
+++ src/mai/java/com/calculadoraGitFlow/miapp/Figuras.java
@@ -0,0 +1,13 @@
+package mai.java.com.calculadoraGitFlow.miapp;
+
+public class Figuras {
+
+	public static double areaCuadrado (double ancho, double largo) {
+		return ancho*largo;
+	}
+	
+	public static double dencidadCubo (double ancho, double largo, double profundidad) {
+		return ancho*largo*profundidad;
+	}
+	
+}
